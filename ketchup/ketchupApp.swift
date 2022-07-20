00e79089036fd95dcfa34b8a562394014c460ce1
ramdanks <ramadhan.kalih@ui.ac.id>
2022-07-20 13:34:33 +0700

    first commit

---
+++ ketchup/ketchupApp.swift
@@ -0,0 +1,17 @@
+//
+//  ketchupApp.swift
+//  ketchup
+//
+//  Created by Ramadhan Kalih Sewu on 20/07/22.
+//
+
+import SwiftUI
+
+@main
+struct ketchupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
